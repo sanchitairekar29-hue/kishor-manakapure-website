2feab1f2860d99ad1cf0d4c0ff72b1f6243dca22
Kishor Manakpure <sanchitairekar29@gmail.com>
2026-08-09 11:31:02 +0530

    Add sitemap for Google

--- app.py
+++ app.py
@@ -117,6 +117,46 @@ def contact():
 def sahityik_vatchal():
     return render_template("sahityik-vatchal.html")
 
+@app.route("/sitemap.xml")
+def sitemap():
+    sitemap_xml = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+
+    <url>
+        <loc>https://kishor-manakapure-website.onrender.com/</loc>
+    </url>
+
+    <url>
+        <loc>https://kishor-manakapure-website.onrender.com/author</loc>
+    </url>
+
+    <url>
+        <loc>https://kishor-manakapure-website.onrender.com/books</loc>
+    </url>
+
+    <url>
+        <loc>https://kishor-manakapure-website.onrender.com/awards</loc>
+    </url>
+
+    <url>
+        <loc>https://kishor-manakapure-website.onrender.com/gallery</loc>
+    </url>
+
+    <url>
+        <loc>https://kishor-manakapure-website.onrender.com/sahityik-vatchal</loc>
+    </url>
+
+    <url>
+        <loc>https://kishor-manakapure-website.onrender.com/contact</loc>
+    </url>
+
+</urlset>
+"""
+
+    return sitemap_xml, 200, {
+        "Content-Type": "application/xml"
+    }
+
 
 if __name__ == "__main__":
     app.run(
