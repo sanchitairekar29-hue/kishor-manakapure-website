4076a884f68f3a2bdf9a3dbb910cd326ba7e794b
Kishor Manakpure <sanchitairekar29@gmail.com>
2026-08-10 19:35:10 +0530

    Fix sitemap

--- app.py
+++ app.py
@@ -117,39 +117,31 @@ def contact():
 def sitemap():
     sitemap_xml = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
 <url>
 <loc>https://kishor-manakapure-website.onrender.com/</loc>
 </url>
-
 <url>
 <loc>https://kishor-manakapure-website.onrender.com/author</loc>
 </url>
-
 <url>
 <loc>https://kishor-manakapure-website.onrender.com/books</loc>
 </url>
-
 <url>
 <loc>https://kishor-manakapure-website.onrender.com/awards</loc>
 </url>
-
 <url>
 <loc>https://kishor-manakapure-website.onrender.com/gallery</loc>
 </url>
-
 <url>
 <loc>https://kishor-manakapure-website.onrender.com/sahityik-vatchal</loc>
 </url>
-
 <url>
 <loc>https://kishor-manakapure-website.onrender.com/contact</loc>
 </url>
-
 </urlset>"""
 
     return sitemap_xml, 200, {
-        "Content-Type": "application/xml"
+        "Content-Type": "application/xml; charset=utf-8"
     }
 
 
